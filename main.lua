7a3875fe18dc5bdd7a179a7faaa406d096ce9214
Unseeing <mokiolover090@gmail.com>
2026-04-13 19:03:56 -0400

    first commit

---
+++ main.lua
@@ -0,0 +1,14 @@
+local love = require("love")
+
+-- Define variables here, local please.
+local text
+
+function love.load()
+    text = "fuck"
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
