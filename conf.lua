a6099026c16e63f75ef5075f279e009806f839f9
Unseeing <mokiolover090@gmail.com>
2026-04-18 22:45:08 -0400

    WHAT THE FUCK.

--- conf.lua
+++ conf.lua
@@ -4,5 +4,6 @@ function love.conf(t)
     t.window.title = "Proyecto Integrado"
     t.window.width = 640 -- You can honestly put any size that scales to modern resolutions but this is the recommended.
     t.window.height = 360
+    t.window.fullscreen = true
     t.window.fullscreentype = "desktop"
 end
--- main.lua
+++ main.lua
@@ -3,14 +3,12 @@ local Player = require("player")
 local utils = require("utils")
 
 -- Declare variables here, local please.
-local fullscreen
 local desktop_width, desktop_height
 local scale_x, scale_y
 
 -- For pre-loading. Loads stuff after loading modules.
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest") -- Makes it so pixel art doesn't look blurry as shit.
-    fullscreen = false
     desktop_width, desktop_height = love.window.getDesktopDimensions()
     scale_x = desktop_width / love.graphics.getWidth()
     scale_y = desktop_height / love.graphics.getHeight()
@@ -21,9 +19,7 @@ function love.update(dt)
 end
 
 function love.draw()
-    if fullscreen then
-        love.graphics.scale(scale_x, scale_y)
-    end
+    love.graphics.scale(scale_x, scale_y)
     Player:draw()
 end
 
@@ -32,9 +28,5 @@ end
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
-    end
-    if key == "f11" then
-        fullscreen = not fullscreen
-        love.window.setFullscreen(fullscreen, "desktop") -- Desktop fullscreen means borderless, better for the players, worse for us.
     end
 end
