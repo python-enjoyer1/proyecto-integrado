157a7a0a5c955ed41c0d4b9fad4fa6a47843d267
Unseeing <mokiolover090@gmail.com>
2026-05-04 21:38:07 -0400

    One deletion.

--- stuff/shaders/background.fs
+++ stuff/shaders/background.fs
@@ -2,7 +2,3 @@
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
     return vec4(1, 1, 1, 1);
 }
-
-vec4 position(mat4 transform_projection, vec4 vertex_position) {
-    return vec4(1, 1, 1, 1);
-}
