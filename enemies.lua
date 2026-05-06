f22ae9cd0eb40370a850a6ee48b43e993f2f3391
rehctiwS <switcherspersonal@gmail.com>
2026-05-06 17:09:55 -0400

    punch + walls

--- enemies.lua
+++ enemies.lua
@@ -19,8 +19,10 @@ Main.Enemy = {
         attack_damage = 5,
         soul_amount = love.math.random(consts.MIN_ENEMY_SOUL, consts.MAX_ENEMY_SOUL),
         essence_amount = love.math.random(consts.MIN_ENEMY_ESSENCE, consts.MAX_ENEMY_ESSENCE),
-        weight = 10,
-        stun_duration = 0
+        weight = 1, --R base weight is 1, 2 or higher is for big enemies
+        stun_duration = 0,
+        knockback_velx = 0,
+        knockback_vely = 0
     },
     states = {
         idle = false,
@@ -29,7 +31,7 @@ Main.Enemy = {
     animation = enemy_walk,
     angle = 0,
     min_distance = 30,
-    hitbox = {x = 100, y = 100, width = consts.CHARACTER_SIZE / 2, height = consts.CHARACTER_SIZE / 2, types = {"hitbox"}}
+    hitbox = {x = 100, y = 100, width = consts.CHARACTER_SIZE / 2, height = consts.CHARACTER_SIZE / 2, types = {"hitbox", "enemycollisionbox"}}
 }
 
 -- This is just so we can have inheritance between different enemy variations.
@@ -67,31 +69,37 @@ function Main.Enemy:update(dt, target)
         self.animation.current_frame = 1
     end
 
-    self.hitbox.x = self.position.x
-    self.hitbox.y = self.position.y
-
     --utils.check_collision(self.hitbox, target.hitbox) R dont do this
 
     if target.punch_hurtbox.active then
-        if utils.check_collision(self.hitbox, target.punch_hurtbox) then
+        --R so enemy doesnt get fucking comboed in 1 punch
+        if not self.hit_this_swing and utils.check_collision(self.hitbox, target.punch_hurtbox) then
+            self.hit_this_swing = true
             self.stats.stun_duration = default_stun
-            if target.punch_hurtbox.x - target.hitbox.x < 0 then
-                self.position.x = utils.lerp(self.position.x, self.position.x - target.stats.knockback / self.stats.weight, 4, dt)
-            else
-                self.position.x = utils.lerp(self.position.x, self.position.x + target.stats.knockback / self.stats.weight, 4, dt)
-            end
 
-            if target.punch_hurtbox.y - target.hitbox.y < 0 then
-                self.position.y = utils.lerp(self.position.y, self.position.y - target.stats.knockback / self.stats.weight, 4, dt)
-            else
-                self.position.y = utils.lerp(self.position.y, self.position.y + target.stats.knockback / self.stats.weight, 4, dt)
-            end
+            local angle = math.atan2(self.position.y - target.position.y, self.position.x - target.position.x)
+            local force = target.stats.knockback / self.stats.weight
+            self.stats.knockback_velx = math.cos(angle) * force
+            self.stats.knockback_vely = math.sin(angle) * force
         end
+    else
+        self.hit_this_swing = false
+    end
+
+    --R knockback = velocity rn, lerp was tp'ing the enemy
+    if math.abs(self.stats.knockback_velx) > 0.1 or math.abs(self.stats.knockback_vely) > 0.1 then
+        self.position.x = self.position.x + self.stats.knockback_velx * dt
+        self.position.y = self.position.y + self.stats.knockback_vely * dt
+        self.stats.knockback_velx = self.stats.knockback_velx * (1 - 10 * dt)
+        self.stats.knockback_vely = self.stats.knockback_vely * (1 - 10 * dt)
     end
 
     self.angle = math.atan2(target.position.y - self.position.y, target.position.x - self.position.x)
 
     self.animation:update(dt, self.states.idle)
+
+    self.hitbox.x = self.position.x
+    self.hitbox.y = self.position.y
 end
 
 function Main.Enemy:draw()
