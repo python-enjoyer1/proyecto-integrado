5974cb118b896c95d9bbdf7e09dede0d172a3751
rehctiwS <switcherspersonal@gmail.com>
2026-06-02 19:03:07 -0400

    small change

--- main.lua
+++ main.lua
@@ -105,8 +105,7 @@ function love.load()
         local x = love.math.random(consts.TILE_SIZE, map_w - consts.TILE_SIZE)
         local y = love.math.random(consts.TILE_SIZE, map_h - consts.TILE_SIZE)
         if love.math.random(1,2) == 1 then
-            print("hi")
-            table.insert(enemy_table, enemies.Enemy:new({position = {x = x, y = y}, stats = {hp = 10, speed = 150}}))
+            table.insert(enemy_table, enemies.Enemy:new({position = {x = x, y = y}, stats = {hp = 10, speed = 150}})) --R sprinters
         else
             table.insert(enemy_table, enemies.Enemy:new({position = {x = x, y = y}, stats = {hp = 20, speed = 100}}))
         end
