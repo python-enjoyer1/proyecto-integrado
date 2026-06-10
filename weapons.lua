c4e525e815db75b73c881d44a6946ee0d66669a0
Unseeing <mokiolover090@gmail.com>
2026-06-09 21:52:18 -0400

    Hm.

--- weapons.lua
+++ weapons.lua
@@ -23,7 +23,7 @@ function Main.HeavyGun:new(o)
     o.rotation = love.math.random(1, 360)
     o.floor_sprite = o.floor_sprite or love.graphics.newImage(consts.WEAPON_PATH .. "range/heavy_gun_floor.png")
     o.hold_sprite = o.hold_sprite or love.graphics.newImage(consts.WEAPON_PATH .. "range/heavy_gun_hold.png")
-    o.interact_box = {x = o.position.x, y = o.position.y, width = 55/2, height = 32/2, types = {"interactbox"}}
+    o.interact_box = {x = o.position.x, y = o.position.y, width = 24, height = 12, types = {"interactbox"}}
     o.sprite = o.floor_sprite
     o.render = true
 
@@ -42,10 +42,10 @@ function Main.HeavyGun:draw(offset_x, offset_y)
     if self.render then
         if set.shading then
             love.graphics.setColor(consts.SHADOW_COLOR)
-            love.graphics.draw(self.sprite, self.position.x, self.position.y + 2.0, math.rad(self.rotation), 0.5, 0.5)
+            love.graphics.draw(self.sprite, self.position.x, self.position.y + 2.0, math.rad(self.rotation), 0.5, 0.5, self.interact_box.width / 2, self.interact_box.height / 2)
             love.graphics.setColor(1, 1, 1)
         end
-        love.graphics.draw(self.sprite, self.position.x, self.position.y, math.rad(self.rotation), 0.5, 0.5)
+        love.graphics.draw(self.sprite, self.position.x, self.position.y, math.rad(self.rotation), 0.5, 0.5, self.interact_box.width / 2, self.interact_box.height / 2)
     end
     if set.debug then
         utils.draw_collision(self.interact_box)
