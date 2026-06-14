d1cd5455b28633a6d55ab3654bb9ddfe525dd775
rehctiwS <switcherspersonal@gmail.com>
2026-06-14 01:24:23 -0400

    name change

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 local love = require("love")
 
 function love.conf(t)
-    t.window.title = "metRo_gOre_26"
+    t.window.title = "METRO ANIMUS"
     t.window.width = 640
     t.window.height = 360
     t.window.fullscreen = true
