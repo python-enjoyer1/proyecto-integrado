0ed22346af7fc82652ed79bb47abadac9c9d1dd1
rehctiwS <switcherspersonal@gmail.com>
2026-06-14 20:53:22 -0400

    stuff

---
+++ build.lua
@@ -0,0 +1,7 @@
+return {
+    name = "METRO ANIMUS",      -- Change this to your game's name
+    version = "0.5",          -- Your version
+    output = "dist",          -- The folder where the game will be created
+    love = "11.5",            -- Make sure this matches your LÖVE version
+    icon = "icon.png"
+}
